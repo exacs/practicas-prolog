793eb51eae6212b5a6efaba5ab75a498951d9c77
Carlos Saito <c.saito@alumnos.upm.es>
2016-11-06 18:25:57 +0100

    Plantilla de la entrega de la práctica 1

---
+++ prac1/prac1.pl
@@ -0,0 +1,10 @@
+alumno_prode('Sabina', 'Hidalgo', 'Irene', 'v130xxx').
+alumno_prode('Saito', 'Murata', 'Carlos', 'v130215').
+alumno_prode('Vargas', 'Azpitarte', 'Daniel', 'v130xxx').
+% Run-length encoding modificado
+comprimir(L0, LC).
+descomprimir(L0, LC).
+
+% Exploración de árboles n-arios
+menores(Arbol, Max).
+suma(Arbol, Suma).
